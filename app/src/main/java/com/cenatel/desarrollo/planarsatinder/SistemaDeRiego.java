4f016cc738e004ae4b28b6ef9889a05a4c2da7dc
simaski <klomp60@gmail.com>
2015-07-29 16:11:19 -0430

    agregado fragment sistemas de riego

---
+++ app/src/main/java/com/cenatel/desarrollo/planarsatinder/SistemaDeRiego.java
@@ -0,0 +1,41 @@
+package com.cenatel.desarrollo.planarsatinder;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by San Casimiro on 29/07/2015.
+ */
+public class SistemaDeRiego extends Fragment implements LocationListener {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_datos, container, false);
+        ((MainActivity) getActivity()).setActionBarTitle("Planarsat Inder");
+        return v;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
