679973107fb199064957cad58be1b563d77bb9c9
simaski <klomp60@gmail.com>
2015-07-30 13:49:45 -0430

    agregado fragment sistemas de riego y su nombre

--- app/src/main/java/com/cenatel/desarrollo/planarsatinder/SistemaDeRiego.java
+++ app/src/main/java/com/cenatel/desarrollo/planarsatinder/SistemaDeRiego.java
@@ -15,7 +15,7 @@ public class SistemaDeRiego extends Fragment implements LocationListener {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_datos, container, false);
-        ((MainActivity) getActivity()).setActionBarTitle("Planarsat Inder");
+        ((MainActivity) getActivity()).setActionBarTitle("Sistema de Riego");
         return v;
     }
 
